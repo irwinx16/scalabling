3d837ae2a57fcd1da4bf15e8c030bcf1e5d7f647
irwin <irgranados16@gmail.com>
2020-01-08 16:47:16 -0600

    form working

--- parking/urls.py
+++ parking/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.get_address, name="get_address"),
+    path("", views.get_address),
+    path("thanks/", views.get_address),
 ]
--- parking/views.py
+++ parking/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+from .forms import AddressForm
 
 def get_address(request):
-    return HttpResponse('get_address view')
+
+    if request.method == 'POST':
+        form = AddressForm(request.POST)
+        if form.is_valid():
+
+            addr = form.cleaned_data['add_field']
+
+            context = {
+            'address': addr
+            }
+
+            template = loader.get_template('thanks.html')
+
+            return HttpResponse(template.render(context, request))
+
+    else:
+        form = AddressForm()
+
+    return render(request, 'form.html', {'form':form})
