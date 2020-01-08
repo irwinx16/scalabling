f4a0fc4b59c9cc4c748c58f0cff6e19e2ee57b6a
irwin <irgranados16@gmail.com>
2020-01-08 15:23:10 -0600

    set up url, views and form

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('blog/', include('blog.urls')),
+    path('parking/', include('parking.urls')),
 ]
