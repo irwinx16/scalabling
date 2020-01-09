1ded7996aa5f30aeee8fd913374e38a5e81a869f
irwin <irgranados16@gmail.com>
2020-01-09 09:12:07 -0600

    add url to parking from homepage

--- parking/urls.py
+++ parking/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.get_address),
+    path("", views.get_address, name="parking_index"),
     path("thanks/", views.get_address),
 ]
