984d93e9291049bb32aeee5d1e4847e1ed9af68d
irwin <irgranados16@gmail.com>
2020-01-09 09:19:02 -0600

    add label to form field

--- parking/forms.py
+++ parking/forms.py
@@ -3,6 +3,7 @@ from django import forms
 class AddressForm(forms.Form):
     add_field = forms.CharField(
         max_length=60,
+        label='Address',
         widget=forms.TextInput(attrs={
             "class": "form-control",
             "placeholder": "123 N Main St"
