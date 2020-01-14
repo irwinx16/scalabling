e1ca73db4a69097e1dfc08a9e1fbc54ccc44b578
irwin <irgranados16@gmail.com>
2020-01-14 17:58:20 -0600

    get only address for even or odd num

--- parking/views.py
+++ parking/views.py
@@ -14,18 +14,31 @@ def get_address(request):
             data = form.cleaned_data['add_field']
             addr = data.upper().split(' ')
 
+            st_num = int(addr[0])
+            st_dir = addr[1]
+            st_name = addr[2]
+            st_type = addr[3]
+
+            # is the address odd or even
+            odd_even = ''
+            if st_num % 2 == 0:
+                odd_even = 'E'
+            else:
+                odd_even = 'O'
+
             #call chicago api
             url = 'https://data.cityofchicago.org/resource/u9xt-hiju.json?'
 
             #pass params for api
-            params = dict(street_direction=addr[1], street_name=addr[2],street_type=addr[3])
+            params = dict(street_direction=st_dir, street_name=st_name,street_type=st_type, odd_even=odd_even)
 
             #get and save response in json
             res = requests.get(url, params=params).json()
 
             for field in res:
-                if (int(field['address_range_low']) <= int(addr[0]) and int(field['address_range_high']) >= int(addr[0]) ):
+                if (int(field['address_range_low']) <= st_num and int(field['address_range_high']) >= st_num ):
                     print(field)
+
 
             context = {
             'st_num': addr[0],
